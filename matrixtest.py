1c3a550655ad7f248e84493f73cbbf51a34244df
Giuseppe Michetti <michetti.@husky.neu.edu>
2021-04-19 15:08:50 -0400

    updated test structure

--- matrixtest.py
+++ matrixtest.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+import os
+p=Path(os.path.dirname(__file__))
+import sys
+sys.path.append(str(p.parent.absolute()))
+
 import sketch
 import numpy as np
 
@@ -11,28 +17,31 @@ device_params["ProbeGroundPadSize"]=500
 
 device.import_params(device_params)
 
-# array=sketch.ParametricArray()
-#
-# array.device=device
-# array.x_spacing=200
-#
+array=sketch.ParametricArray()
+
+array.device=device
+array.x_spacing=200
+
+param1=sketch.LayoutParam({"IDTCoverage":[0.3,0.5,0.7]})
+param2=sketch.LayoutParam({"BusSize":[sketch.Point(x,100) for x in [50,100,150]]})
+
+array.x_param=(param1.combine(param2))()
+
 # array.x_param={"IDTCoverage": [ _ for _ in np.arange(0.2,1,0.2)],\
-#     "IDTPitch":[_ for _ in np.arange(5,25,5)] }
-#
-# array.auto_labels(top_label="HAHA",bottom_label="HEY")
-#
-# array.test(size=200,spacing=200)
+#     "IDTcitch":[_ for _ in np.arange(5,25,5)] }
 
-mat=sketch.ParametricMatrix("Hi")
+array.auto_labels()
 
-mat.x_spacing=200
-mat.y_spacing=400
+array.test(size=200,spacing=200)
 #
-# mat.labels_top=None
-# mat.labels_bottom=None
-
-mat.device=device
-mat.auto_labels(col_index=3,row_index=3)
-# print(mat.labels_top)
-# print(mat.labels_bottom)
-mat.test(size=200,spacing=50)
+# mat=sketch.ParametricMatrix("Hi")
+# mat.x_spacing=200
+# mat.y_spacing=400
+# # mat.labels_top=None
+# # mat.labels_bottom=None
+#
+# mat.device=device
+# mat.auto_labels(top_label="HAHA",col_index=3,row_index=3)
+# # print(mat.labels_top)
+# # print(mat.labels_bottom)
+# mat.test(size=200,spacing=50)
