3a63b2d2fe19617a79d1c78c26181a8bfa02c5a9
giumc <michetti.@husky.neu.edu>
2021-12-27 18:03:40 -0500

    added MultiAnchor, fixed inheritance issue in draw using __wrapped__

---
+++ tests/pcell_test.py
@@ -0,0 +1,19 @@
+import phidl.geometry as pg
+import phidl.device_layout as dl
+import pirel.pcells as pc
+import pirel.modifiers as pm
+import pirel.tools as pt
+import pirel.addOns.standard_parts as ps5
+
+
+# a=pc.Anchor()
+# a.view()
+# ma=pc.MultiAnchor()
+# ma.view()
+
+idt=pc.IDT()
+idt.view()
+
+idt_pe=pc.PartialEtchIDT()
+
+idt_pe.view()
